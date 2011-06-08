a6029de1c57c2b70f445704a293103d61d56694d
Luis R. Rodriguez <lrodriguez@atheros.com>
2011-06-08 12:32:38 -0700

    acs: clean up acs.h
    Remove a bunch of unused stuff inherited from iw.
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>

--- acs.h
+++ acs.h
@@ -22,40 +22,6 @@ struct nl80211_state {
 	struct genl_family *nl80211;
 };
 
-enum command_identify_by {
-	CIB_NONE,
-	CIB_PHY,
-	CIB_NETDEV,
-};
-
-enum id_input {
-	II_NONE,
-	II_NETDEV,
-	II_PHY_NAME,
-	II_PHY_IDX,
-};
-
-struct cmd {
-	const char *name;
-	const char *args;
-	const char *help;
-	const enum nl80211_commands cmd;
-	int nl_msg_flags;
-	int hidden;
-	const enum command_identify_by idby;
-	/*
-	 * The handler should return a negative error code,
-	 * zero on success, 1 if the arguments were wrong
-	 * and the usage message should and 2 otherwise.
-	 */
-	int (*handler)(struct nl80211_state *state,
-		       struct nl_cb *cb,
-		       struct nl_msg *msg,
-		       int argc, char **argv);
-	const struct cmd *(*selector)(int argc, char **argv);
-	const struct cmd *parent;
-};
-
 struct freq_item {
 	__u16 center_freq;
 	struct list_head list_member;
@@ -92,55 +58,6 @@ struct survey_item {
 extern const char acs_version[];
 
 extern int nl_debug;
-
-int handle_cmd(struct nl80211_state *state, enum id_input idby,
-	       int argc, char **argv);
-
-struct print_event_args {
-	struct timeval ts; /* internal */
-	bool have_ts; /* must be set false */
-	bool frame, time, reltime;
-};
-
-__u32 listen_events(struct nl80211_state *state,
-		    const int n_waits, const __u32 *waits);
-int __prepare_listen_events(struct nl80211_state *state);
-__u32 __do_listen_events(struct nl80211_state *state,
-			 const int n_waits, const __u32 *waits,
-			 struct print_event_args *args);
-
-
-int mac_addr_a2n(unsigned char *mac_addr, char *arg);
-void mac_addr_n2a(char *mac_addr, unsigned char *arg);
-int parse_hex_mask(char *hexmask, unsigned char **result, size_t *result_len,
-		   unsigned char **mask);
-unsigned char *parse_hex(char *hex, size_t *outlen);
-
-int parse_keys(struct nl_msg *msg, char **argv, int argc);
-
-void print_ht_mcs(const __u8 *mcs);
-void print_ampdu_length(__u8 exponent);
-void print_ampdu_spacing(__u8 spacing);
-void print_ht_capability(__u16 cap);
-
-const char *iftype_name(enum nl80211_iftype iftype);
-const char *command_name(enum nl80211_commands cmd);
-int ieee80211_channel_to_frequency(int chan);
-int ieee80211_frequency_to_channel(int freq);
-
-void print_ssid_escaped(const uint8_t len, const uint8_t *data);
-
-int nl_get_multicast_id(struct nl_sock *sock, const char *family, const char *group);
-
-char *reg_initiator_to_string(__u8 initiator);
-
-const char *get_reason_str(uint16_t reason);
-const char *get_status_str(uint16_t status);
-
-enum print_ie_type {
-	PRINT_SCAN,
-	PRINT_LINK,
-};
 
 int handle_survey_dump(struct nl80211_state *state,
 		       struct nl_cb *cb);
